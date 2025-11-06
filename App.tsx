9fd549b510da289b041d8a70c423aa8d5395ce91
tsaqifmuwaffaq@gmail.com <tsaqifmuwaffaq@gmail.com>
2025-11-06 15:51:07 +0700

    day1 week2

--- App.tsx
+++ App.tsx
@@ -1,26 +1,91 @@
-// import React from 'react';
-import * as React from 'react';
+import { Pressable, StyleSheet, Text, View } from 'react-native';
+import BoxThreeColor from './src/components/BoxThreeColor';
 import { useState } from 'react';
 
-import { SafeAreaView, StatusBar, StyleSheet } from 'react-native';
-import { globalStyles } from './src/styles/globalStyles';
-import { HomeScreen } from './src/screens/HomeScreen';
+const App = () => {
+  const [flexDirection, setFlexDirection] = useState('row');
+  const [justifyContent, setJustifyContent] = useState('center');
+  const [alignItems, setAlignItems] = useState('center');
 
-
-const App: React.FC = () => {
   return (
-   <SafeAreaView style={globalStyles.container}>
-   <StatusBar barStyle="dark-content" backgroundColor="#f9f9f9" />
-      <HomeScreen />
-    </SafeAreaView>
+    <View style={styles.body}>
+      <Text style={styles.text}>Tugas No 1</Text>
+      <BoxThreeColor
+        flexDirection={flexDirection as 'row' | 'column'}
+        justifyContent={
+          justifyContent as 'flex-start' | 'center' | 'space-between'
+        }
+        alignItems={alignItems as 'flex-start' | 'center' | 'flex-end'}
+      />
+      <Text style={styles.text}>Tugas No 2</Text>
+      <Pressable
+        style={styles.button}
+        onPress={() =>
+          setFlexDirection(prev => (prev === 'row' ? 'column' : 'row'))
+        }
+      >
+        <Text style={styles.buttonText}>Change Direction</Text>
+      </Pressable>
+      <Text style={styles.text}>Tugas No 3</Text>
+      <Pressable
+        style={styles.button}
+        onPress={() => {
+          setJustifyContent(prev => {
+            if (prev === 'flex-start') return 'center';
+            if (prev === 'center') return 'space-between';
+            return 'flex-start';
+          });
+        }}
+      >
+        <Text style={styles.buttonText}>Change Justify Content</Text>
+      </Pressable>
+      <Text style={styles.text}>Tugas No 4</Text>
+      <Pressable
+        style={styles.button}
+        onPress={() => {
+          setAlignItems(prev => {
+            if (prev === 'flex-start') return 'center';
+            if (prev === 'center') return 'flex-end';
+            return 'flex-start';
+          });
+        }}
+      >
+        <Text style={styles.buttonText}>Change Align Items</Text>
+      </Pressable>
+    </View>
   );
 };
 
-export default App;
-
 const styles = StyleSheet.create({
-  container: {
+  body: {
     flex: 1,
-    backgroundColor: '#f9f9f9',
+    backgroundColor: 'white',
+  },
+  text: {
+    fontSize: 20,
+    textAlign: 'center',
+    marginTop: 10,
+    marginBottom: 20,
+    fontWeight: 'bold',
+    color: '#A72703',
+  },
+  button: {
+    width: 200,
+    height: 50,
+    backgroundColor: '#CD2C58',
+    borderRadius: 25,
+    alignSelf: 'center',
+    justifyContent: 'center',
+    alignItems: 'center',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 5,
+  },
+  buttonText: {
+    color: 'white',
+    fontSize: 18,
   },
 });
+
+export default App;
